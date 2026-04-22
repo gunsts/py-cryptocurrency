b5170c834b64b66af9a241ef2e42196dfb3a42f2
gunsts <137734013+gunsts@users.noreply.github.com>
2026-04-23 06:48:35 +0700

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -21,3 +21,24 @@ def test_exchange_rate_prediction_in_boundary(
     # mock exchange rate prediction within -0.5% and 0.5% boundary
     monkeypatch.setattr(main, "get_exchange_rate_prediction", lambda x: 1.01)
     assert main.cryptocurrency_action(1.0) == "Do nothing"
+
+
+def test_should_do_nothing_when_decrease_is_exactly_5_percent(
+        monkeypatch: pytest.MonkeyPatch) -> None:
+    # 95/100 = 0.95 (NOT more than 5%)
+    monkeypatch.setattr(main, "get_exchange_rate_prediction", lambda x: 95.0)
+    assert main.cryptocurrency_action(100) == "Do nothing"
+
+
+def test_should_do_nothing_when_change_is_less_than_5_percent(
+        monkeypatch: pytest.MonkeyPatch) -> None:
+    # rate in boundaries
+    monkeypatch.setattr(main, "get_exchange_rate_prediction", lambda x: 100.0)
+    assert main.cryptocurrency_action(100) == "Do nothing"
+
+
+def test_should_do_nothing_when_increase_exactly_5_percent(
+        monkeypatch: pytest.MonkeyPatch) -> None:
+    # exactly 5 percent increase
+    monkeypatch.setattr(main, "get_exchange_rate_prediction", lambda x: 105.0)
+    assert main.cryptocurrency_action(100) == "Do nothing"
