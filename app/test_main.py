5c0bd50aa295b90870b12ab31576ef6f7beeb6d0
gunsts <137734013+gunsts@users.noreply.github.com>
2026-04-22 21:51:29 +0700

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,23 @@
-# write your code here
+import pytest
+from app import main
+
+
+def test_exchange_rate_prediction_increase(
+        monkeypatch: pytest.MonkeyPatch) -> None:
+    # mock exchange rate prediction increase more than 5%
+    monkeypatch.setattr(main, "get_exchange_rate_prediction", lambda x: 1.1)
+    assert main.cryptocurrency_action(1.0) == "Buy more cryptocurrency"
+
+
+def test_exchange_rate_prediction_decrease(
+        monkeypatch: pytest.MonkeyPatch) -> None:
+    # mock exchange rate prediction decrease more than 5%
+    monkeypatch.setattr(main, "get_exchange_rate_prediction", lambda x: 0.8)
+    assert main.cryptocurrency_action(1.0) == "Sell all your cryptocurrency"
+
+
+def test_exchange_rate_prediction_in_boundary(
+        monkeypatch: pytest.MonkeyPatch) -> None:
+    # mock exchange rate prediction within -0.5% and 0.5% boundary
+    monkeypatch.setattr(main, "get_exchange_rate_prediction", lambda x: 1.01)
+    assert main.cryptocurrency_action(1.0) == "Do nothing"
